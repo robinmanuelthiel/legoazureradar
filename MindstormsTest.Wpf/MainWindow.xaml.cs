aa3270309db4f57d7c502a4ee5452bb979adfedc
Robin-Manuel Thiel <robin-manuel@thiel1.de>
2016-10-07 16:38:38 +0200

    Enhance UI

--- MindstormsTest.Wpf/MainWindow.xaml.cs
+++ MindstormsTest.Wpf/MainWindow.xaml.cs
@@ -12,51 +12,21 @@ namespace MindstormsTest.Wpf
     public partial class MainWindow : Window
     {
         private DeviceClient deviceClient;
-        private Brick mindstormsBrick;        
+        private Brick mindstormsBrick;
         private DispatcherTimer timer;
 
-        private string iotHubUri = "legoiothub.azure-devices.net";
-        private string deviceId = "LEGOEV3";
-        private string deviceKey = "G35+JOKSUHYXcxZ8DD6VEwsbdUHAAXg5Ld48lYzmTBM=";
+        //private string iotHubUri = "legoiothub.azure-devices.net";
+        //private string deviceId = "LEGOEV3";
+        //private string deviceKey = "G35+JOKSUHYXcxZ8DD6VEwsbdUHAAXg5Ld48lYzmTBM=";
         private int Volume = 100;
 
         public MainWindow()
         {
             InitializeComponent();
-            Loaded += MainPage_Loaded;
-
-            try
-            {
-                Log("Establishing Azure connection...", false);
-                deviceClient = DeviceClient.Create(iotHubUri, new DeviceAuthenticationWithRegistrySymmetricKey(deviceId, deviceKey), TransportType.Mqtt);
-                Log(" success!");
-            }
-            catch (Exception)
-            {
-                Log(" failed!");
-            }
-
-            mindstormsBrick = new Brick(new UsbCommunication());
-            mindstormsBrick.BrickChanged += MindstormsBrick_BrickChanged;
 
             timer = new DispatcherTimer();
             timer.Interval = TimeSpan.FromMilliseconds(1000);
             timer.Tick += Timer_Tick;
-        }
-
-        private async void MainPage_Loaded(object sender, RoutedEventArgs e)
-        {            
-            try
-            {
-                Log("Connecting with EV3 Brick via USB...", false);
-                await mindstormsBrick.ConnectAsync();
-                Log(" success!");
-            }
-            catch (Exception)
-            {
-                Log("EV3 Brick connection failed :(");
-                Log(" failed!");
-            }
         }
 
         private void MindstormsBrick_BrickChanged(object sender, BrickChangedEventArgs e)
@@ -72,12 +42,12 @@ namespace MindstormsTest.Wpf
             var now = DateTime.UtcNow;
             var distance = mindstormsBrick.Ports[InputPort.One].SIValue;
             Log($"{now} - Distance: {distance.ToString()}");
-            
+
             if (distance < 255)
             {
-                // Play near sound  
+                // Play near sound
                 await mindstormsBrick.DirectCommand.PlayToneAsync(100, 1000, 300);
-            }       
+            }
             else
             {
                 // Play far sound
@@ -85,7 +55,7 @@ namespace MindstormsTest.Wpf
             }
 
             // Format data to a JSON message
-            var telemetryDataPoint = new { id = deviceId, distance = distance, dateTime = now };
+            var telemetryDataPoint = new { id = tbkDeviceId.Text, distance = distance, dateTime = now };
             var json = JsonConvert.SerializeObject(telemetryDataPoint);
             var message = new Message(Encoding.ASCII.GetBytes(json));
 
@@ -100,6 +70,62 @@ namespace MindstormsTest.Wpf
                 LogText.Text += "\n";
 
             LogText.ScrollToEnd();
+        }
+
+        private void slVolume_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            Volume = (int)e.NewValue;
+        }
+
+        private async void Start_Click(object sender, EventArgs e)
+        {
+            bool running = true;
+            tbkIotHubUrl.IsEnabled = false;
+            tbkDeviceId.IsEnabled = false;
+            tbkDeviceKey.IsEnabled = false;
+
+            try
+            {
+                Log("Establishing Azure connection...", false);
+                deviceClient = DeviceClient.Create(tbkIotHubUrl.Text, new DeviceAuthenticationWithRegistrySymmetricKey(tbkDeviceId.Text, tbkDeviceKey.Text), TransportType.Mqtt);
+                Log(" success!\n");
+            }
+            catch (Exception)
+            {
+                Log(" failed!");
+                Log("Please check the Azure IoT Hub URL, Device ID or Device Key.\n");
+                running = false;
+            }
+
+            try
+            {
+                Log("Connecting with EV3 Brick via USB...", false);
+                mindstormsBrick = new Brick(new UsbCommunication());
+                mindstormsBrick.BrickChanged += MindstormsBrick_BrickChanged;
+                await mindstormsBrick.ConnectAsync();
+                Log(" success!");
+            }
+            catch (Exception)
+            {
+                Log(" failed!");
+                Log("Please check the USB connection and switch the EV3 on.\n");
+                running = false;
+            }
+
+            if (!running)
+            {
+                tbkIotHubUrl.IsEnabled = true;
+                tbkDeviceId.IsEnabled = true;
+                tbkDeviceKey.IsEnabled = true;
+            }
+        }
+
+        private void Stop_Click(object sender, EventArgs e)
+        {
+            timer.Stop();
+            tbkIotHubUrl.IsEnabled = true;
+            tbkDeviceId.IsEnabled = true;
+            tbkDeviceKey.IsEnabled = true;
         }
     }
 }
